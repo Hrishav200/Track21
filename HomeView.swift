5dcbab01bfd18c7af47192ffc0701c3ecaf99709
Hrishav Sunar <hrishavsunar@gmail.com>
2025-10-13 21:49:57 +1100

    Hello Github

---
+++ HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Track21
+//
+//  Created by Hrishav Sunar on 13/10/2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello from mac studio m4 pro max")
+    }
+}
+
+#Preview {
+    HomeView()
+}
